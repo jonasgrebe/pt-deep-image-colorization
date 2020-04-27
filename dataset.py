03909b2164c95923bb307a870f3c1556c5be087c
Jonas Grebe <26788162+jonasgrebe@users.noreply.github.com>
2020-04-27 18:40:32 +0200

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -9,25 +9,24 @@ import matplotlib.pyplot as plt
 import cv2
 
 
-def load_single_image_lab(load_path):
-    img = cv2.imread(load_path)
-    return cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+class ImageDatasetLAB(torch.utils.data.Dataset):
 
-def save_single_image_lab(save_path, img):
-    img = img.astype('uint8')
-    img = cv2.cvtColor(img, cv2.COLOR_LAB2BGR)
-    cv2.imwrite(save_path, img)
+    def __init__(self, data_dir: str, transform: Callable = None) -> None:
+        """ ImageDatasetL2AB: Dataset class to handle a flat image directory.
 
+        Parameters
+        ----------
+        data_dir : str
+            Path to the flat image directory. This directory should not contain any other subdirectory or unsupported file types.
+        transform : Callable
+            Callable that takes in a PIL image and returns the transformation result.
 
-class ImageLABDataset(torch.utils.data.Dataset):
-
-    def __init__(self, data_dir: str, pil_transform: Callable = None, pt_transform: Callable = None) -> None:
-        super(ImageLABDataset, self).__init__()
+        """
+        super(ImageDatasetLAB, self).__init__()
 
         self.data_dir = data_dir
         self.img_files = os.listdir(self.data_dir)
-        self.pil_transform = pil_transform
-        self.pt_transform = pt_transform
+        self.transform = transform
 
 
     def __len__(self) -> int:
@@ -41,38 +40,92 @@ class ImageLABDataset(torch.utils.data.Dataset):
         if idx >= len(self):
             raise IndexError
 
+        # load the image at the given index
         load_path = os.path.join(self.data_dir, self.img_files[idx])
-        img = load_single_image_lab(load_path)
+        img = cv2.imread(load_path)
 
+        # translate the image to the LAB color space
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        # convert image to PIL image for pytorch transformations
         img = torchvision.transforms.functional.to_pil_image(img)
-        if self.pil_transform:
-            img = self.pil_transform(img)
 
+        # apply transformation if necessary
+        if self.transform:
+            img = self.transform(img)
+
+        # convert image back to numpy and translate into LAB color space
+
+        if torch.is_tensor(img):
+            img = np.array(img)
+            img = img.transpose(1, 2, 0)
+        else:
+            img = np.array(img)
+
+
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2LAB)
+
+        # convert numpy.ndarray to torch.Tensor
         img = torchvision.transforms.functional.to_tensor(img)
-        if self.pt_transform:
-            img = self.pt_transform(img)
 
-        return img[0:1], img[1:]
+        return img
+
+
+def save_single_image_lab(save_path, img):
+    img = img.astype('uint8')
+    img = cv2.cvtColor(img, cv2.COLOR_LAB2BGR)
+    cv2.imwrite(save_path, img)
+
+
+def fill_lab_img(L=None, AB=None):
+    if L is None and AB is None:
+        raise ValueError
+
+    if L is None:
+        return np.concatenate([np.zeros((1,) + AB.shape[1:]), AB], axis=0)
+    else:
+        return np.concatenate([L, np.zeros((2,) + L.shape[1:])], axis=0)
+
+
+
 
 
 if __name__ == '__main__':
-    dataset = ImageLABDataset('data/square-custom-unsplash-10K',
-                              pil_transform=torchvision.transforms.Compose([
-                                #torchvision.transforms.RandomCrop(384),
-                                torchvision.transforms.Resize(32),
-                              ]),
-                              pt_transform=torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)))
 
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=10000, shuffle=False, num_workers=0)
+    dataset = ImageDatasetLAB('data/square-custom-unsplash-10K', )
 
-    for step, batch in enumerate(dataloader):
-        L = batch[0]
-        AB = batch[1]
+    n_val = 100
 
-        LAB = torch.cat([L, AB], dim=1)
+    train_dataset, val_dataset = torch.utils.data.random_split(dataset, (len(dataset)-n_val, n_val))
 
-        grid = torchvision.utils.make_grid(LAB, nrow=200)
-        grid = grid * 127.5 + 127.5
-        save_single_image_lab('test.png', grid.numpy().transpose((1, 2, 0)))
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=8, shuffle=False, num_workers=0)
 
-        exit(1)
+    for step, sample in enumerate(dataloader):
+
+        # EXAMPLE FOR VALIDATION ROUTINE
+        # get image from batch
+        img_batch = sample['img']
+        file_batch = sample['img_file']
+
+        # transform to (0, 1)
+        img_batch = transform_input(img_batch)
+
+        # extract L and AB channels
+        L_batch, AB_batch = img_batch[:,:1], img_batch[:,1:]
+
+        print(L_batch.size(), AB_batch.size())
+
+        # fill the other one with zeros for each of them
+        L_batch, AB_batch = np.concatenate([L_batch, np.zeros_like(AB_batch)], axis=1), np.concatenate([np.zeros_like(L_batch), AB_batch], axis=1)
+
+        print(L_batch.shape, AB_batch.shape)
+
+        for img, L, AB, file in zip(img_batch, L_batch, AB_batch, file_batch):
+
+            # show all of them together with the image
+            x = np.concatenate([img, L, AB], axis=2).transpose(1, 2, 0)
+            x = transform_output(x)
+
+            x = x.astype('uint8')
+            x = cv2.cvtColor(x, cv2.COLOR_LAB2BGR)
+            cv2.imwrite(save_path, x)
